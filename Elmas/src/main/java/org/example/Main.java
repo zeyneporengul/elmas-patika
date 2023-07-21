9e178970779e95491a4ea3e4547c4f4db2a575f5
zeyneporengul <102181813+zeyneporengul@users.noreply.github.com>
2023-07-22 00:12:25 +0300

    Add files via upload

---
+++ Elmas/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int counter;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        counter = input.nextInt();
+
+        for (int i = 1; i <= counter; i++) {
+            for( int k = 1; k <= counter-i; k++){
+                System.out.print(" ");
+            } for (int p = 1; p<=(2*i)-1; p++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = counter; i >=0; i--) {
+            for( int k = 1; k <= counter-i; k++){
+                System.out.print(" ");
+            } for (int p = 1; p<=(2*i)-1; p++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
